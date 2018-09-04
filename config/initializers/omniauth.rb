85d1a339f17cf91be3c968f65e69ed5d415c7095
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-09-04 11:59:18 +0530

    files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1965811946774333', 'c5ccef03c58c7508651db1d285e98625'
+  provider :facebook, '454513015034954', '4b48e9d4570039e4ed50103ade6ab690'
   provider :linkedin, '783sdajz9q4k5r', 'XTVnhHC4FUQVc6DH'
   #provider :google, '731848018382-g16kobi6mlshoitti2rqaopc787absl5.apps.googleusercontent.com', 'TqFATE0Se1U3X1shKVczoXWm', scope: 'plus.login'
   provider :twitter, 'y6kILe3VwuGpaFIIfPXdnv8pw', 'Gb76xJS6ZvTrzzxp0jNFLnZG2nNhNaaNPfx5FAWLrL5y11e14D'
